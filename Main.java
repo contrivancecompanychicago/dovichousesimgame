434e1a487948c194b3e613f8d6a0533eae8be15d
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-10 11:51:28 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 package com.dovichousesimgame;
 
-public class Main {
+ class dovichousesimgameMain {
+   public static void main(string[] args) {
+     System.out.println("This is DOVIC House Sim game!");
+   }
 }
