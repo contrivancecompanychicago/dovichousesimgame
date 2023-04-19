a93e3a08c6f063fe9d0cd473b1515dea02a5c4ae
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-19 15:20:06 -0500

    Update SmeagolLotrenemyActor.java

--- SmeagolLotrenemyActor.java
+++ SmeagolLotrenemyActor.java
@@ -1,5 +1,39 @@
-public class SmeagolLotrenemyActor extends Explorer 
-{
+package greenfoot.*;
 
+public class SmeagolLotrenemyActor implements Explorer 
+{
+  public void movearound()
+  {
+    move(4);
+    turn(Greenfoot.getRandomNumber(96));
+    }
+    
+    public void movearound()
+    {
+     move(4);
+     if (Greenfoot.getRandomNumber(96) < (8)
+     {
+      turn(Greenfoot.getRandomNumber(72));
+     }
+    }
+    public void movearound()
+    {
+      move(4);
+      if (Greenfoot.getRandomNumber(96) < (8)
+      {
+       turn(Greenfoot.getRandomNumber(96) - 48);
+      }
+      
+      if (getX() <= 6 || getX() >= getWorld().getWidth() - 6)
+      {
+       turn(190);
+      }
+      
+      if (getY() <= 6 || getY() >= getWorld().getHeight() - 6)
+      
+      {
+        turn(180);
+      }
+      
 }
 
