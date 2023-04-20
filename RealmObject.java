f7d6bcdc9cceacefedd94e6d4cb98d4bb1144598
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-04-20 06:57:59 -0500

    Create RealmObject.java

---
+++ RealmObject.java
@@ -0,0 +1,14 @@
+import java.lang.Object;
+import Realm from "realm";
+
+public abstract class RealmObject {
+  
+}
+
+export const DOVICHouseSimEnvSchema = {
+  name: 'DOVICHouseSimEnv',
+  properties: {
+    _id: 'objectId?',
+  },
+  primaryKey: '_id',
+};
