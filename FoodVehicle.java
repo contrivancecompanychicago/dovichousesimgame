ad3de6a6a2145dc7375578c908c971adf6a4e330
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-05-12 22:24:49 -0500

    Update FoodVehicle.java

--- FoodVehicle.java
+++ FoodVehicle.java
@@ -1,1 +1,22 @@
+package game.vehicles;
 
+import game.characters.Explorer;
+import game.terrains.Scene;
+
+public class FoodVehicle implements Explorer{
+   private String name;
+   public FoodVehicle (String name){
+       this.name = name;
+   }
+   public char getFootprint(){
+        return 'c';
+       
+   }
+   public void explore(scene sceneToExplore){
+	    if (sceneToExplore.findPathFrom(1, 0)) {
+			sceneToExplore.print(getFootprint());
+		} else {
+			System.out.println("no solution found");
+		}
+	}
+}
