22347682c8fadbbb722d0562c9d901f43d0fdd41
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-05-16 06:19:14 -0500

    Create TriforceSimulation.java

---
+++ TriforceSimulation.java
@@ -0,0 +1,35 @@
+public class TriforceSimulation {
+
+    private Triforce triforce;
+    private ArrayList<Molecule> molecules;
+
+    public TriforceSimulation(int n) {
+        triforce = new Triforce(1, 1);
+        molecules = new ArrayList<Molecule>();
+        for (int i = 0; i < n; i++) {
+            molecules.add(new Molecule());
+        }
+    }
+
+    public void step() {
+        for (int i = 0; i < molecules.size(); i++) {
+            for (int j = i + 1; j < molecules.size(); j++) {
+                double r = molecules.get(i).getPosition().distance(molecules.get(j).getPosition());
+                double force = triforce.getForce(r);
+                molecules.get(i).addForce(force);
+                molecules.get(j).addForce(-force);
+            }
+        }
+
+        for (Molecule molecule : molecules) {
+            molecule.updatePosition();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            step();
+        }
+    }
+
+}
