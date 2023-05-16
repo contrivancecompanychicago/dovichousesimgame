415693317628e6d0db853ddd043432f13e8f3d7b
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2023-05-16 06:17:38 -0500

    Create Triforce.java

---
+++ Triforce.java
@@ -0,0 +1,23 @@
+public class Triforce {
+
+    private double epsilon;
+    private double sigma;
+
+    public Triforce(double epsilon, double sigma) {
+        this.epsilon = epsilon;
+        this.sigma = sigma;
+    }
+
+    public double getPotentialEnergy(double r) {
+        return 4 * epsilon * ((sigma / r) ** 12 - (sigma / r) ** 6);
+    }
+
+    public double getForce(double r) {
+        return -12 * epsilon * ((sigma / r) ** 13 - (sigma / r) ** 7);
+    }
+
+    public void draw() {
+        // Draw the Triforce in Java.
+    }
+
+}
